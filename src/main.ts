8d17119f3c18a35d616f01be1009e753aa8f32db
wdxGit <1091830282@qq.com>
2020-09-09 09:52:22 +0800

    项目初始化

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入elemnt-ui组件
+import './utils/element-components';
+
 Vue.config.productionTip = false
 
 new Vue({
